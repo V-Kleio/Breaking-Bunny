57374afdf00ce188c02dbdd7c04ae1513ed18305
khalizkr <156478326+khalizkr@users.noreply.github.com>
2024-01-17 23:19:49 +0700

    Add files via upload

--- BreakingBunny/scripts/collection.gd
+++ BreakingBunny/scripts/collection.gd
@@ -5,8 +5,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,3 +14,38 @@ func _process(delta):
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn") # Replace with function body.
+
+
+func _on_right_pressed():
+	get_tree().change_scene_to_file("res://scenes/collection2.tscn") # Replace with function body.
+
+
+func _on_story_1_pressed():
+	$Story1Card.show()
+
+func _on_story_1_card_pressed():
+	$Story1Card.hide()
+
+
+func _on_story_2_pressed():
+	$Story2Card.show()
+
+
+func _on_gilang_pressed():
+	$GilangCard.show()
+
+
+func _on_city_pressed():
+	$CityCard.show()
+
+
+func _on_story_2_card_pressed():
+	$Story2Card.hide()
+
+
+func _on_city_card_pressed():
+	$CityCard.hide()
+
+
+func _on_gilang_card_pressed():
+	$GilangCard.hide()
